1c4ac3fbd7f42bf812998a14cb2ca990d08774f9
unknown <muhammad.sayyid@enthu-tech.com>
2024-07-02 16:01:45 +0800

    secondcommit

--- testlah/Data/ApplicationDbContext.cs
+++ testlah/Data/ApplicationDbContext.cs
@@ -6,17 +6,18 @@ namespace testlah.Data
     public class ApplicationDbContext : DbContext
     {
         public DbSet<Product> Products { get; set; }
-        //public DbSet<Category> Categories { get; set; }
+        public DbSet<Category> Categories { get; set; }
         public ApplicationDbContext (DbContextOptions<ApplicationDbContext> options) : base(options) { }
-        //protected override void OnModelCreating(ModelBuilder modelBuilder)
-        //{
-        //    modelBuilder.Entity<Product>()
-        //        .HasOne(p => p.Category)
-        //        .WithMany(c => c.Products)
-        //        .HasForeignKey(p => p.CategoryId); // Foreign key in Product table
 
-        //    // Configure other entity mappings if needed
-        //}
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>()
+                .HasOne(p => p.Category)
+                .WithMany(c => c.Products)
+                .HasForeignKey(p => p.CategoryId); // Foreign key in Product table
+
+            // Configure other entity mappings if needed
+        }
     }
 
 }
--- testlah/Model/Product.cs
+++ testlah/Model/Product.cs
@@ -9,9 +9,9 @@ namespace testlah.Model
         public int Id { get; set; }
         public string Name { get; set; }
         public decimal Price { get; set; }
-        
-        //public int CategoryId { get; set; }
-        //public Category Category { get; set; }
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
     }
 
 }
